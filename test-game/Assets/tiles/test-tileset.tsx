531aab2524054903fda5fb1f4154b74b23f9aa82
George Clensy <george.clensy@gmail.com>
2024-07-23 16:04:04 +0100

    Fixed a bug in the tileset

--- test-game/Assets/tiles/test-tileset.tsx
+++ test-game/Assets/tiles/test-tileset.tsx
@@ -1,4 +1,4 @@
-<? xml version = "1.0" encoding = "UTF-8" ?>
+<?xml version = "1.0" encoding = "UTF-8" ?>
   <tileset version="1.10" tiledversion="1.10.2" name="test-tileset" tilewidth="32" tileheight="32" tilecount="18" columns="0">
     <grid orientation="orthogonal" width="1" height="1" />
     <tile id="0">
